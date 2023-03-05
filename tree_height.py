6c372881b856982169f82e67f4c24af7de021a7d
221RDB263 <125553206+enijaub@users.noreply.github.com>
2023-03-05 22:00:50 +0200

    Update tree_height.py
    221RDB263 Enija Undīne Brakše 5 gr.

--- tree_height.py
+++ tree_height.py
@@ -69,4 +69,3 @@ if __name__ == '__main__':
     sys.setrecursionlimit(10**7)  # max depth of recursion
     threading.stack_size(2**27)   # new thread will get stack of such size
     threading.Thread(target=main).start()
-    main()
